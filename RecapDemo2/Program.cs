ac6c19389621c64f48deef5ce9b33c6f1d8b5732
U180709040 <rozerinyildiz@posta.mu.edu.tr>
2022-07-03 17:24:16 +0300

    a basic implementation using interface

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//create a basic implementation with using interface
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+           // customerManager.Logger = new DatabaseLogger();
+            customerManager.Logger = new FileLogger();
+            customerManager.Add();
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        
+            public ILogger Logger { get; set; }
+            public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer added.");
+        }
+           
+    }
+
+    // if only class logger, (no interface ect) you can use it only with new()
+    // db'e de basılabilir veya txt ye json formatında da eklenebilir
+    class DatabaseLogger: ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database!");
+        }
+    }
+
+    class FileLogger: ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file!");
+        }
+    }
+
+    class SMSLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to SMS!");
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+}
